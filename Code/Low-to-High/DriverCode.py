47795dc9fb028cbc1a65220539c4117e3026d36b
Shinde, Darshan Gopal <dshinde@iu.edu>
2019-04-05 17:42:36 -0400

    Low-to-High GAN model
    Uploading code for Low-to-High generator model and its dependencies like residual block and special 3x3 convolution.
    
    Uploading driver code as well.

---
+++ Code/Low-to-High/DriverCode.py
@@ -0,0 +1,110 @@
+from __future__ import print_function
+import argparse
+import os
+import random
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+import torch.utils.data
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+import torchvision.utils as vutils
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from Low_to_High_Generator import Generator
+from IPython.display import HTML
+
+
+# Set random seem for reproducibility
+manualSeed = 999
+#manualSeed = random.randint(1, 10000) # use if you want new results
+print("Random Seed: ", manualSeed)
+random.seed(manualSeed)
+torch.manual_seed(manualSeed)
+
+# Root directory for dataset
+dataroot = "F:/Study/2nd_Semester/CV/Project/celeba-dataset"
+
+# Number of workers for dataloader
+workers = 0
+
+# Batch size during training
+batch_size = 128
+
+# Spatial size of training images. All images will be resized to this
+#   size using a transformer.
+image_size = 64
+
+# Number of channels in the training images. For color images this is 3
+nc = 3
+
+# Size of z latent vector (i.e. size of generator input)
+nz = 100
+
+# Size of feature maps in generator
+ngf = 64
+
+# Size of feature maps in discriminator
+ndf = 64
+
+# Number of training epochs
+num_epochs = 5
+
+# Learning rate for optimizers
+lr = 0.0002
+
+# Beta1 hyperparam for Adam optimizers
+beta1 = 0.5
+
+# Number of GPUs available. Use 0 for CPU mode.
+ngpu = 1
+
+# We can use an image folder dataset the way we have it setup.
+# Create the dataset
+dataset = dset.ImageFolder(root=dataroot,
+                           transform=transforms.Compose([
+                               transforms.Resize(image_size),
+                               transforms.CenterCrop(image_size),
+                               transforms.ToTensor(),
+                               transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                           ]))
+
+# Create the dataloader
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
+                                         shuffle=True, num_workers=workers)
+
+# Decide which device we want to run on
+device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
+
+# Plot some training images
+real_batch = next(iter(dataloader))
+plt.figure(figsize=(8,8))
+plt.axis("off")
+plt.title("Training Images")
+plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=2, normalize=True).cpu(),(1,2,0)))
+
+# custom weights initialization called on netG and netD
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
+        
+# Create the generator
+netG = Generator(ngpu).to(device)
+
+# Handle multi-gpu if desired
+if (device.type == 'cuda') and (ngpu > 1):
+    netG = nn.DataParallel(netG, list(range(ngpu)))
+    
+# Apply the weights_init function to randomly initialize all weights
+#  to mean=0, stdev=0.2.
+netG.apply(weights_init)
+
+# Print the model
+print(netG)
---
+++ Code/Low-to-High/Low_to_High_Generator.py
@@ -0,0 +1,79 @@
+import torch.nn as nn
+from Convolution import conv3x3
+from ResidualBlock import ResidualBlock
+
+class Generator(nn.Module):
+    def __init__(self, ngpu=1):
+        super(Generator, self).__init__()
+        self.ngpu = ngpu
+
+        res_units = [256, 128, 96]
+        inp_res_units = [
+            [256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256,
+             256], [256, 128, 128], [128, 96, 96]]
+
+        self.layers_set = []
+        self.layers_set_up = []
+        self.layers_set_final = nn.ModuleList()
+        self.layers_set_final_up = nn.ModuleList()
+
+        self.a1 = nn.Sequential(nn.Conv2d(256, 128, 1, 1))
+        self.a2 = nn.Sequential(nn.Conv2d(128, 96, 1, 1))
+        self.layers_in = conv3x3(3, 256)
+
+        layers = []
+
+        for ru in range(len(res_units) - 1):
+            nunits = res_units[ru]
+            curr_inp_resu = inp_res_units[ru]
+            self.layers_set.insert(ru, [])
+            self.layers_set_up.insert(ru, [])
+
+            if ru == 0:
+                num_blocks_level = 12
+            else:
+                num_blocks_level = 3
+
+            for j in range(num_blocks_level):
+                # if curr_inp_resu[j]==3:
+                self.layers_set[ru].append(ResidualBlock(curr_inp_resu[j], nunits))
+                # else:
+                # layers.append(MyBlock(curr_inp_resu[j], nunits))
+
+            self.layers_set_up[ru].append(nn.Upsample(scale_factor=2, mode='bilinear',align_corners=True))
+            self.layers_set_up[ru].append(nn.BatchNorm2d(nunits))
+            self.layers_set_up[ru].append(nn.ReLU(True))
+            self.layers_set_up[ru].append(nn.ConvTranspose2d(nunits, nunits, kernel_size=1, stride=1))
+            self.layers_set_final.append(nn.Sequential(*self.layers_set[ru]))
+            self.layers_set_final_up.append(nn.Sequential(*self.layers_set_up[ru]))
+
+        nunits = res_units[-1]
+        layers.append(conv3x3(inp_res_units[-1][0], nunits))
+        layers.append(nn.ReLU(True))
+        layers.append(nn.Conv2d(inp_res_units[-1][1], nunits, kernel_size=1, stride=1))
+        layers.append(nn.ReLU(True))
+        layers.append(nn.Conv2d(nunits, 3, kernel_size=1, stride=1))
+        layers.append(nn.Tanh())
+
+        self.main = nn.Sequential(*layers)
+
+    def forward(self, input):
+        x = self.layers_in(input)
+
+        for ru in range(len(self.layers_set_final)):
+            if ru == 0:
+                temp = self.layers_set_final[ru](x)
+                x = x + temp
+            elif ru == 1:
+                temp = self.layers_set_final[ru](x)
+                temp2 = self.a1(x)
+                x = temp + temp2
+            elif ru == 2:
+                temp = self.layers_set_final[ru](x)
+                temp2 = self.a2(x)
+                x = temp + temp2
+
+            x = self.layers_set_final_up[ru](x)
+
+        x = self.main(x)
+        return x
